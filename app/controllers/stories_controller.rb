330d97abf511cff7cc88b60e06861abfda9c0c73
juliaccyang <juliayjunjun@gmail.com>
2022-03-23 20:27:58 +0800

    finish story update

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,44 @@
+class StoriesController < ApplicationController
+
+    before_action :authenticate_user!
+    before_action :find_story, only: [:edit, :update, :destroy]
+
+    def index
+        @stories = current_user.stories.order(created_at: :desc)
+    end
+
+    def new
+        @story = current_user.stories.new
+    end
+
+    def create
+        @story = current_user.stories.new(story_params)
+
+        if @story.save
+            redirect_to stories_path, notice: 'Successfully Add Story!'
+        else
+        render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @story.update(story_params)
+            redirect_to stories_path, notice: 'Successfully Update Story!'
+        else
+            render :edit
+        end
+    end
+
+    private
+    def find_story
+        @story = current_user.stories.find(params[:id])
+    end
+
+    def story_params
+        params.require(:story).permit(:title, :content)
+    end
+end
